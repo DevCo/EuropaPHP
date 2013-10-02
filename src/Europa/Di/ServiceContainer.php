f673bc324586966c1c74d0fe1e1071f441bc3020
Trey Shugart <treshugart@gmail.com>
2013-10-02 16:11:26 +1000

    Add ability to use ::get() and remove unnecessary static key generation.

--- src/Europa/Di/ServiceContainer.php
+++ src/Europa/Di/ServiceContainer.php
@@ -26,33 +26,13 @@ class ServiceContainer implements ServiceContainerInterface
         if (isset($this->services[$name])) {
             return call_user_func_array($this->services[$name], $args);
         }
-        
+
         Exception::toss('Cannot invoke service "%s" in container "%s" because it does not exist.', $name, $this->name());
     }
 
     public function __set($name, $value)
     {
-        // If a string is given, it is assumed to just be a class.
-        if (is_string($value)) {
-            $value = function() use ($value) {
-                return new $value;
-            };
-        }
-
-        // Anything that is not a closure is returned by a closure.
-        if (!$value instanceof Closure) {
-            $value = function() use ($value) {
-                return $value;
-            };
-        }
-
-        // Blow away cache if it exists.
-        if (isset($this->cache[$name])) {
-            unset($this->cache[$name]);
-        }
-
-        // Rebind the closure to the container.
-        $this->services[$name] = $value->bindTo($this);
+        $this->register($name, $value);
     }
 
     public function __get($name)
@@ -93,6 +73,33 @@ class ServiceContainer implements ServiceContainerInterface
     public function configure(callable $configuration)
     {
         $configuration($this);
+        return $this;
+    }
+
+    public function register($name, $value)
+    {
+        // If a string is given, it is assumed to just be a class.
+        if (is_string($value)) {
+            $value = function() use ($value) {
+                return new $value;
+            };
+        }
+
+        // Anything that is not a closure is returned by a closure.
+        if (!$value instanceof Closure) {
+            $value = function() use ($value) {
+                return $value;
+            };
+        }
+
+        // Blow away cache if it exists.
+        if (isset($this->cache[$name])) {
+            unset($this->cache[$name]);
+        }
+
+        // Rebind the closure to the container.
+        $this->services[$name] = $value->bindTo($this);
+
         return $this;
     }
 
@@ -152,23 +159,8 @@ class ServiceContainer implements ServiceContainerInterface
 
     public function save($name)
     {
-        self::$instances[self::generateKey($name)] = $this;
+        self::$instances[$name] = $this;
         return $this;
-    }
-
-    public static function __callStatic($name, array $args = [])
-    {
-        $key = self::generateKey($name);
-
-        if (!$args && isset(self::$instances[$key])) {
-            return self::$instances[$key];
-        }
-        
-        try {
-            return self::$instances[$key] = (new ClassReflector(get_called_class()))->newInstanceArgs($args);
-        } catch (\Exception $e) {
-            Exception::toss('Could not get the container "%s" from "%s" because: %s', $name, get_called_class(), $e->getMessage());
-        }
     }
 
     protected function throwNotExists($name)
@@ -184,8 +176,17 @@ class ServiceContainer implements ServiceContainerInterface
         Exception::toss($message, $name, $this->fullName());
     }
 
-    public static function generateKey($name)
+    public static function __callStatic($name, array $args = [])
     {
-        return get_called_class() . '::' . $name . '()';
+        return static::get($name);
+    }
+
+    public static function get($name = 'main')
+    {
+        if (isset(self::$instances[$name])) {
+            return self::$instances[$name];
+        }
+
+        throw new \RuntimeException(sprintf('The container "%s" does not exist.', $name));
     }
 }
