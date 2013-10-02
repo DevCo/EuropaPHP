82c1c73e3fbd70754a18d860bae9bdbed2f2c415
Trey Shugart <treshugart@gmail.com>
2013-10-02 16:11:36 +1000

    If quotes are around an argument, remove them.

--- src/Europa/Request/Cli.php
+++ src/Europa/Request/Cli.php
@@ -6,36 +6,36 @@ use Europa\Filter\ToStringFilter;
 class Cli extends RequestAbstract implements CliInterface
 {
     private $commands = array();
-    
+
     public function __construct()
     {
         $this->setMethod(static::METHOD);
         $this->parseCommands();
         $this->parseParams();
     }
-    
+
     public function __toString()
     {
         return trim($this->getCommand() . ' ' . $this->getParamsAsString());
     }
-    
+
     public function setCommand($command)
     {
         $this->commands = preg_split('/\s+/', $command);
         return $this;
     }
-    
+
     public function getCommand()
     {
         return implode(static::T_NS, $this->getCommands());
     }
-    
+
     public function setCommands(array $commands)
     {
         $this->commands = $commands;
         return $this;
     }
-    
+
     public function getCommands()
     {
         return $this->commands;
@@ -52,45 +52,49 @@ class Cli extends RequestAbstract implements CliInterface
 
         return trim($params);
     }
-    
+
     private function parseCommands()
     {
         $args = (isset($_SERVER['argv'])) ? $_SERVER['argv'] : array();
         $cmds = array();
-        
+
         array_shift($args);
-        
+
         foreach ($args as $arg) {
             if (strpos($arg, '-') === 0) {
                 break;
             }
-            
+
             $this->commands[] = $arg;
         }
-        
+
         return $this;
     }
-    
+
     private function parseParams()
     {
         $args = (isset($_SERVER['argv'])) ? $_SERVER['argv'] : array();
         $skip = false;
-        
+
         array_shift($args);
-        
+
         foreach ($args as $index => $param) {
             if ($skip) {
                 $skip = false;
                 continue;
             }
-            
+
             // allow dash prefixing
             if ($param[0] === '-') {
                 $cut   = $param[1] === '-' ? 2 : 1;
                 $param = substr($param, $cut, strlen($param));
                 $next  = isset($args[$index + 1]) ? $args[$index + 1] : false;
-                
+
                 if ($next !== false && $next[0] !== '-') {
+                    if ($next[0] === '"') {
+                        $next = substr($next, 1, -1);
+                    }
+
                     $this->setParam($param, $next);
                     $skip = true;
                 } else {
@@ -98,7 +102,7 @@ class Cli extends RequestAbstract implements CliInterface
                 }
             }
         }
-        
+
         return $this;
     }
 }
